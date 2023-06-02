729f756ca0d752ddca048afbe481527c120e911d
JJ <jenniferj1796@gmail.com>
2023-06-02 11:45:09 -0400

    added webpack and src-sw

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 // process.env.PORT  NEEDED FOR HEROKU
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
